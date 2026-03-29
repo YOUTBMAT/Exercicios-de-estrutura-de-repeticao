fcfd1aa3a2d3c25a1f55870e0502c627fb0a8888
Mateus Weiss Medeiros <mateuswmedeiros@gmail.com>
2026-03-29 20:50:46 -0300

    Ex de pares e impares esta feito

---
+++ Ex/Pares e Impares.py
@@ -0,0 +1,19 @@
+loop = 10
+
+
+par = []
+impar = []
+while True:
+    numero = int(input("Digite o número: "))
+    if numero % 2 == 0:
+        # numero = par
+        par.append(numero)
+    else:
+        # numero = impar
+        impar.append(numero)
+
+    loop = loop -1
+    if loop == 0:
+        break
+print ("A lista de números pares é",par)
+print ("A lista de números ímpares é",impar)
