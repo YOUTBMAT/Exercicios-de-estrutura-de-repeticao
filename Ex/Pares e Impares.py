fa1f2b8666073e0c0b65b1c87b9a0a4d8827ea46
Mateus Weiss Medeiros <mateuswmedeiros@gmail.com>
2026-03-30 09:31:09 -0300

    Pares e ímpares estava fazendo algo diferente do pedido, adicionado o que foi pedido, mas mantido o que tinha por ser interessante

--- Ex/Pares e Impares.py
+++ Ex/Pares e Impares.py
@@ -1,18 +1,20 @@
 loop = 10
 
+quantPar = 0
+quantImp = 0
 par = []
 impar = []
 while True:
     numero = int(input("Digite o número: "))
     if numero % 2 == 0:
-        # numero = par
+        quantPar = quantPar + 1
         par.append(numero)
     else:
-        # numero = impar
+        quantImp = quantImp + 1
         impar.append(numero)
 
     loop = loop -1
     if loop == 0:
         break
-print ("A lista de números pares é",par)
-print ("A lista de números ímpares é",impar)
+print (quantPar,"números são par, a lista de números pares é",par)
+print (quantImp,"números são ímpar, a lista de números ímpares é",impar)
